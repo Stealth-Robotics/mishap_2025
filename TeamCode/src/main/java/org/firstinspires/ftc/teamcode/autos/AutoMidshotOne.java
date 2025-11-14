6d781825c2e7b2a0626174294c3e6e433a88cf66
Go Stealth <stealthrobotics@gmail.com>
2025-11-14 13:24:38 -0800

    midshot auto -LSH

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoMidshotOne.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.paths.Path;
+import org.firstinspires.ftc.teamcode.paths.PathMidShot;
+import org.firstinspires.ftc.teamcode.paths.PathState;
+
+import java.util.Arrays;
+
+@Autonomous(name = "Shoot Mid either Side", group = "Autonomous", preselectTeleOp = "_TeleOp_Driver_Operator")
+@Configurable
+@Disabled
+public class AutoMidshotOne extends AutosDecode {
+
+    @Override
+    protected Path initPaths() {
+        shootIndexes.addAll(Arrays.asList(1, 4));
+        intakeIndexes.addAll(Arrays.asList(3));
+        return new PathMidShot(robot);
+    }
+
+    @Override
+    protected void setSpindexerSlots() {
+        robot.initSpindxerSlotsEmpty();
+    }
+
+    @Override
+    protected void setStartingPose() {
+        // change the angle of the far shots by a couple of degrees:
+        // a negative number turns the bot more to the left positive more to the right
+        if (Alliance.isBlue()) {
+            this.aimOffset = 4;
+        } else {
+            this.aimOffset = -2;
+        }
+
+        Pose startPose = paths.getPathStart();
+        if (lastPose != null) {
+            startPose = startPose.setHeading(lastPose.getHeading());
+        }
+        follower.setStartingPose(startPose);
+    }
+
+    @Override
+    protected PathState checkIndexForAction() {
+        // First, call the base implementation for any common actions
+        PathState baseState = super.checkIndexForAction();
+        if (baseState != PathState.IDLE) {
+            return baseState; // The base class is handling something
+        }
+
+        // TODO: Add any specific actions here for the auto here
+        // shooting and intaking are in in the base class
+
+        return PathState.IDLE;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoMidshotRed.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.paths.Path;
+import org.firstinspires.ftc.teamcode.paths.PathMidShot;
+import org.firstinspires.ftc.teamcode.paths.PathState;
+
+import java.util.Arrays;
+
+@Autonomous(name = "Shoot Mid red Side", group = "Red", preselectTeleOp = "_TeleOp_Driver_Operator")
+@Configurable
+public class AutoMidshotRed extends AutosDecode {
+
+    @Override
+    protected Path initPaths() {
+        shootIndexes.addAll(Arrays.asList(1, 4));
+        intakeIndexes.addAll(Arrays.asList(3));
+        return new PathMidShot(robot);
+    }
+
+    @Override
+    protected void setSpindexerSlots() {
+        robot.initSpindxerSlotsEmpty();
+    }
+
+    @Override
+    protected void setAlliance() {
+        // Set the specific alliance for this OpMode
+        // can use limelight data if you want
+        Alliance.set(Alliance.RED);
+    }
+    @Override
+    protected void setStartingPose() {
+        // change the angle of the far shots by a couple of degrees:
+        // a negative number turns the bot more to the left positive more to the right
+        if (Alliance.isBlue()) {
+            this.aimOffset = 4;
+        } else {
+            this.aimOffset = -2;
+        }
+
+        Pose startPose = paths.getPathStart();
+        if (lastPose != null) {
+            startPose = startPose.setHeading(lastPose.getHeading());
+        }
+        follower.setStartingPose(startPose);
+    }
+
+    @Override
+    protected PathState checkIndexForAction() {
+        // First, call the base implementation for any common actions
+        PathState baseState = super.checkIndexForAction();
+        if (baseState != PathState.IDLE) {
+            return baseState; // The base class is handling something
+        }
+
+        // TODO: Add any specific actions here for the auto here
+        // shooting and intaking are in in the base class
+
+        return PathState.IDLE;
+    }
+
+}
